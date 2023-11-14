f01a7aa70c9c6a34f932570559346d5a8ea7225d
mzelley <colymarielaure@gmail.com>
2023-11-14 09:43:33 +0000

    Début gestion notes pour le professeur

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardAdminComponent } from './dashboard-admin/dashboard-admin.component';
 import { AuthComponent } from './auth/auth.component';
+import { GestionNotesComponent } from './gestion-notes/gestion-notes.component';
 
 const routes: Routes = [
   {path: 'dashboard', component:DashboardAdminComponent},
   {path: 'auth', component:AuthComponent},
   {path: '', redirectTo:'auth', pathMatch:'full'},
+  {path:'gestionnotes', component:GestionNotesComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
-// import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardAdminComponent } from './dashboard-admin/dashboard-admin.component';
 import { AuthComponent } from './auth/auth.component';
 import { FormsModule } from '@angular/forms';
+import { GestionNotesComponent } from './gestion-notes/gestion-notes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardAdminComponent,
-    AuthComponent
+    AuthComponent,
+    GestionNotesComponent
   ],
   imports: [
     BrowserModule,
